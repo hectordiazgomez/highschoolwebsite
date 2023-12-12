7cb3ff425644ce608788853148e1ea11d02174e6
hectordiazgomez <konlaptechs@gmail.com>
2023-12-11 23:37:21 -0500

    menu fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
 
 const Header = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false); // State to toggle menu
+
+
     return(
         <>
-        <div className="hidden sm:flex w-full h-8 bg-blue-700 ">
+            <div className="hidden sm:flex w-full h-8 bg-blue-700">
         </div>
-
         <div className="bg-blue-900 border-b-2 border-blue-800 px-8 flex">
         <div className="w-1/3 sm:w-1/4 py-8 flex justify-center sm:justify-end">
         <Link to="/"><img className="sm:w-40 w-32 h-32 sm:h-40" src="https://luya.blob.core.windows.net/test/logo_colegio_iesm.svg" alt="svgimage" /></Link>
@@ -19,9 +23,24 @@ const Header = () => {
         <div className="w-1/3 sm:flex justify-center hidden">
             <img src="https://luya.blob.core.windows.net/hermes/Estudiantes.png" alt="" className="w-1/2 h-auto py-6"/>
         </div>
+                <div className="sm:hidden flex items-center">
+                    <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="text-white">
+                        {isMenuOpen ? <XIcon className="h-8 w-8" /> : <MenuIcon className="h-8 w-8" />}
+                    </button>
+                </div>
         </div>
+            {isMenuOpen && (
+                <div className="px-5 bg-blue-900 flex flex-col sm:hidden">
+                    {/* Repeat your Link components here */}
+                    <Link to="/" className="text-white py-2">Publicaciones</Link>
+                    <Link to="/videos" className="text-white py-2">Galería</Link>
+                    <Link to="/history" className="text-white py-2">Historia</Link>
+                    <Link to="/authorities" className="text-white py-2">Autoridades</Link>
+                    <Link to="/login" className="text-white py-2">Administrador</Link>
+                </div>
+            )}
 
-        <div className="px-5 bg-blue-900 flex">
+        <div className="px-5 bg-blue-900 flex sm:flex-row hidden">
         <div className="w-2/5 hidden sm:flex justify-center">
         <Link to="/"><p className="text-white cursor-pointer hover:text-gray-200 py-3 text-lg font-semibold">Publicaciones</p></Link>
         </div>
